9e9cc5e9489fcabe1bf083cad73b8c21b26c7451
崔大洋 <cuidayang@timeface.cn>
2016-11-22 16:04:31 +0800

    a

--- TFCoreFoundation/ViewController.m
+++ TFCoreFoundation/ViewController.m
@@ -24,7 +24,7 @@
 }
 
 - (NSArray*)titlesForViewControllers {
-    return @[@"VCA",@"VCB",@"VCC",];
+    return @[@"VCA",@"VCBCCCCBBBB",@"VCC",];
 }
 
 - (UIViewController *)viewControllerAtIndex:(NSUInteger)index {
